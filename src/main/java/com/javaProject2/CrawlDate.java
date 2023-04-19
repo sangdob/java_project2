5072990fd11af2dbbd9110c038ee3dc6e9e51562
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-19 13:23:18 +0900

    feat : [202]Issue CrawlDateTime - CrawlDate class 추가 - localDateTime Object

---
+++ src/main/java/com/javaProject2/CrawlDate.java
@@ -0,0 +1,11 @@
+package com.javaProject2;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
