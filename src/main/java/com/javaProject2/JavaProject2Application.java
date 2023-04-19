589554424aa2b2cf1685194eb0df767d22556357
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-19 11:20:44 +0900

    first Commit -- Spring boot Project

---
+++ src/main/java/com/javaProject2/JavaProject2Application.java
@@ -0,0 +1,13 @@
+package com.javaProject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaProject2/JavaProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
