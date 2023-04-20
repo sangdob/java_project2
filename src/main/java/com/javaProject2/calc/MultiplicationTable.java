5822c9e9582d49680e52d151ad257203106390bb
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-20 11:31:22 +0900

    feat : 구구단2단과 printf실습 - PrintTwoValuesShortage class 추가 - MultiplicationTable 구구단 class 추가

---
+++ src/main/java/com/javaProject2/calc/MultiplicationTable.java
@@ -0,0 +1,12 @@
+package com.javaProject2.calc;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+//        2 * 1 = 2
+//        2 * 2 = 4
+        for (int i = 0; i < 9; i++) {
+            int num = i + 1;
+            System.out.printf("2 * %d = %d\n", num, 2 * num);
+        }
+    }
+}
---
+++ src/main/java/com/javaProject2/input/PrintTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.javaProject2.input;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. 그런데 제가 하고 싶은 말은 %s 입니다.", "박상도", "오늘은 좋은날씨");
+    }
+}
