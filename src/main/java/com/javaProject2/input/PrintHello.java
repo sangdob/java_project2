7934a5724f37f10e6d4412a496f7f580b89950e1
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-20 15:19:52 +0900

    feat : 변수 실습 코드 작성 - VariableEx.java

---
+++ src/main/java/com/javaProject2/input/PrintHello.java
@@ -0,0 +1,7 @@
+package com.javaProject2.input;
+
+public class PrintHello {
+    public void print() {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/com/javaProject2/val/VariableEx.java
@@ -0,0 +1,16 @@
+package com.javaProject2.val;
+
+import com.javaProject2.input.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+
+        PrintHello printHello;
+        printHello = new PrintHello();
+
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
