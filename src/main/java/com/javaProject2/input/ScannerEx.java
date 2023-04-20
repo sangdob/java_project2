a0b3c53694b76d167197e11c414966b30a7173e8
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-20 09:22:53 +0900

    feat : Scanner를 이용한 두 수 더하기 - plusNumbersAddTest Method 추가

--- src/main/java/com/javaProject2/input/ScannerEx.java
+++ src/main/java/com/javaProject2/input/ScannerEx.java
@@ -10,7 +10,11 @@ public class ScannerEx {
     public void readTwoNumbersAndPlus() {
         // 숫자 2개 입력 받기
         Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
 
+    public void readTwoNumberAndPlusTest() {
+        Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextInt() + sc.nextInt());
     }
 }
--- src/main/java/com/javaProject2/input/ScannerExTest.java
+++ src/main/java/com/javaProject2/input/ScannerExTest.java
@@ -3,6 +3,7 @@ package com.javaProject2.input;
 public class ScannerExTest {
     public static void main(String[] args) {
         ScannerEx scannerEx = new ScannerEx();
-        scannerEx.readTwoNumbersAndPlus();
+//        scannerEx.readTwoNumbersAndPlus();
+        scannerEx.readTwoNumberAndPlusTest();
     }
 }
