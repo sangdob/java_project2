c568049c5fbe179de57b5add22bec96f7138028c
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-20 13:41:03 +0900

    feat : int avg 실습 + float format형식 실습

---
+++ src/main/java/com/javaProject2/calc/FloatAvgEx.java
@@ -0,0 +1,10 @@
+package com.javaProject2.calc;
+
+import java.util.Scanner;
+
+public class FloatAvgEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.2f", (sc.nextInt() + sc.nextInt() + sc.nextInt()) / 3);
+    }
+}
---
+++ src/main/java/com/javaProject2/calc/FloatFormatEx.java
@@ -0,0 +1,7 @@
+package com.javaProject2.calc;
+
+public class FloatFormatEx {
+    public static void main(String[] args) {
+        System.out.printf("%.1f", 0.5);
+    }
+}
