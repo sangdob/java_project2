6af9f43801d7c3e9152bee6154b030697d974d41
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-21 00:09:55 +0900

    feat : 변수 및 instance 실습

---
+++ src/main/java/com/javaProject2/val/DefinedAndInitialize.java
@@ -0,0 +1,12 @@
+package com.javaProject2.val;
+
+import com.javaProject2.input.PrintHello;
+
+import java.util.Scanner;
+
+public class DefinedAndInitialize {
+    int iVal = 1;
+    PrintHello printHello = new PrintHello();
+
+    Scanner sc = new Scanner(System.in);
+}
