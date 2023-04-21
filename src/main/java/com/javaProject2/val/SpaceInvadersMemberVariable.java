77ec53512b8b2f30051db19fc1ff02c5e3e43175
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-21 14:20:34 +0900

    feat : SpaceInvadersMemberVariable 멤버변수 실습

---
+++ src/main/java/com/javaProject2/val/SpaceInvadersMemberVariable.java
@@ -0,0 +1,29 @@
+package com.javaProject2.val;
+
+public class SpaceInvadersMemberVariable {
+    private int location = 0; // 멤버변수
+
+    public void moveLeft() {
+        location -= 1;
+        System.out.printf("moveLeft: %d\n", location);
+    }
+    public void moveRight() {
+        location += 1;
+        System.out.printf("moveRight: %d\n", location);
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+
+    public static void main(String[] args) {
+        SpaceInvadersMemberVariable simv = new SpaceInvadersMemberVariable();
+        simv.moveLeft();
+        for (int i = 0; i < 4; i++) {
+            simv.moveRight();
+        }
+
+        System.out.println(simv.getLocation());
+    }
+}
