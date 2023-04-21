abd1fbe0515a68567fcdb0975d50e258a093331f
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-21 15:15:55 +0900

    feat : 유저 정보 출력 테스트

---
+++ src/main/java/com/javaProject2/dto/User.java
@@ -0,0 +1,48 @@
+package com.javaProject2.dto;
+
+public class User {
+    private String name;
+    private int age;
+    private String email;
+
+    protected User() {}
+
+    public User(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/javaProject2/test/UserTest.java
@@ -0,0 +1,10 @@
+package com.javaProject2.test;
+
+import com.javaProject2.dto.User;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("parsangdo", 27, "sangdob@gmail.com");
+        user.toString();
+    }
+}
