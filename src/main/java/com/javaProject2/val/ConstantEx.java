76fbd4b3791afbfd7fefffce85b26143ad533f3d
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-21 11:16:45 +0900

    feat : 변수 및 상수 실습

---
+++ src/main/java/com/javaProject2/val/ConstantEx.java
@@ -0,0 +1,12 @@
+package com.javaProject2.val;
+
+public class ConstantEx {
+    public static void main(String[] args) {
+        final int account = 2000;
+
+        System.out.println("account = " + account);
+
+//        final로 지정되어 상수로 처리됨
+//        account = 100;
+    }
+}
---
+++ src/main/java/com/javaProject2/val/MyAccountEx.java
@@ -0,0 +1,12 @@
+package com.javaProject2.val;
+
+public class MyAccountEx {
+    public static void main(String[] args) {
+        int sumOfMyAccount = 1000;
+
+        System.out.println("내 통장에는 " + sumOfMyAccount + "원이 들어있습니다.");
+
+        sumOfMyAccount = 2000;
+        System.out.println("내 통장에는 " + sumOfMyAccount + "원이 들어있습니다.");
+    }
+}
