07917be73a3356a83676f3b7ebd65ed3a28a6cc6
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-24 21:38:44 +0900

    feat : Java 기본타입 실습

---
+++ src/main/java/com/javaProject2/w2/BooleanTypeResult.java
@@ -0,0 +1,11 @@
+package com.javaProject2.w2;
+
+public class BooleanTypeResult {
+    public static void main(String[] args) {
+        boolean result1 = 1 == 1;
+        boolean result2 = 1 == 2;
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+}
---
+++ src/main/java/com/javaProject2/w2/StringConcat.java
@@ -0,0 +1,11 @@
+package com.javaProject2.w2;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String name = "김경록";
+        int balance = 10000;
+        String message = name + "님의 통장 잔고는 " + balance + "원입니다.";
+        System.out.println(message);
+    }
+}
+
