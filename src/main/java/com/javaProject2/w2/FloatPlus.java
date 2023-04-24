868893e7b075ae45708295ef32cf9c53369de749
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-24 13:33:56 +0900

    feat : Java 기본 타입 테스트

---
+++ src/main/java/com/javaProject2/w2/FloatPlus.java
@@ -0,0 +1,11 @@
+package com.javaProject2.w2;
+
+public class FloatPlus {
+    public static void main(String[] args) {
+        String val1 = "1.11111111"; // 소수점 여덟 자리
+        String val2 = "2.11111111"; // 소수점 여덟 자리
+        float result = Float.parseFloat(val1) + Float.parseFloat(val2);
+        System.out.println(result);
+    }
+}
+
---
+++ src/main/java/com/javaProject2/w2/NarrowingCasting.java
@@ -0,0 +1,12 @@
+package com.javaProject2.w2;
+
+public class NarrowingCasting {
+    public static void main(String[] args) {
+        double dVal = 123.456;
+// double 타입인 d를 int 타입으로 강제 형 변환
+        int iVal = (int) dVal;
+        System.out.println("dVal = " + dVal);
+        System.out.println("iVal = " + iVal);
+    }
+}
+
