2723c7b9ba46655759ffa4313c82fd85a50f8800
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-26 11:33:43 +0900

    feat : 2차원 배열 실습 - TwoDimArrSetValue refactoring

--- src/main/java/com/javaProject2/w2/d3/TwoDimArrSetValue.java
+++ src/main/java/com/javaProject2/w2/d3/TwoDimArrSetValue.java
@@ -5,10 +5,18 @@ import java.util.Arrays;
 public class TwoDimArrSetValue {
     public static void main(String[] args) {
         int[][] arr = new int[3][3];
+
+        print(arr);
+
         arr[0][0] = 1;
         arr[1][0] = 2;
         arr[0][2] = 3;
 
+        print(arr);
+
+    }
+
+    private static void print(int[][] arr) {
         System.out.println(Arrays.toString(arr[0]));
         System.out.println(Arrays.toString(arr[1]));
         System.out.println(Arrays.toString(arr[2]));
