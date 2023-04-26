2de790f8cbfb0bc10afdc381aedca9c1305baf84
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-26 10:40:12 +0900

    feat : 2차원 배열 실습 - TenxTen 하드코딩 - TenxTenFor for활용

--- src/main/java/com/javaProject2/w2/d3/TenxTen.java
+++ src/main/java/com/javaProject2/w2/d3/TenxTen.java
@@ -6,8 +6,15 @@ public class TenxTen {
     public static void main(String[] args) {
         int[][] arr10x10 = new int[10][10];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(Arrays.toString(arr10x10[i]));
-        }
+        System.out.println(Arrays.toString(arr10x10[0]));
+        System.out.println(Arrays.toString(arr10x10[1]));
+        System.out.println(Arrays.toString(arr10x10[2]));
+        System.out.println(Arrays.toString(arr10x10[3]));
+        System.out.println(Arrays.toString(arr10x10[4]));
+        System.out.println(Arrays.toString(arr10x10[5]));
+        System.out.println(Arrays.toString(arr10x10[6]));
+        System.out.println(Arrays.toString(arr10x10[7]));
+        System.out.println(Arrays.toString(arr10x10[8]));
+        System.out.println(Arrays.toString(arr10x10[9]));
     }
 }
