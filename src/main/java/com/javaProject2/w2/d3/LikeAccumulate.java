5f39a7d5464ea30549aa257336ebb36cdb46cac7
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-26 15:35:36 +0900

    feat : 대입연산 - 좋아요 누적 실습

---
+++ src/main/java/com/javaProject2/w2/d3/LikeAccumulate.java
@@ -0,0 +1,15 @@
+package com.javaProject2.w2.d3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        likeCount += 1;
+        System.out.println(likeCount);
+
+        likeCount += 1;
+        System.out.println(likeCount);
+
+        likeCount += 1;
+        System.out.println(likeCount);
+    }
+}
