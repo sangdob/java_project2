5974549575686b91594222bed77a84a2e9e2a8e7
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-27 09:24:57 +0900

    feat : 배열에 있는 수 전체 더하기

---
+++ src/main/java/com/javaProject2/w2/d4/ArraySum.java
@@ -0,0 +1,15 @@
+package com.javaProject2.w2.d4;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {2, 1, 7, 4};
+
+        answer += arr[0];
+        answer += arr[1];
+        answer += arr[2];
+        answer += arr[3];
+
+        System.out.println("answer = " + answer);
+    }
+}
