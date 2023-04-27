42880f5eba44c35f93b6134ed7baf3c7823cd931
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-27 10:15:01 +0900

    feat : 증감연산자 실습

---
+++ src/main/java/com/javaProject2/w2/d4/IncreaseFirst.java
@@ -0,0 +1,13 @@
+package com.javaProject2.w2.d4;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 9};
+        idx += 1;
+        System.out.println(arr[idx]);
+        idx += 1;
+        System.out.println(arr[idx]);
+
+    }
+}
