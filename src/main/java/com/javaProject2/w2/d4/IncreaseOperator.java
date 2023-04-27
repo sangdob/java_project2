bb9828897d2811269de6aa98232fb7c4341b13e7
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-27 10:07:55 +0900

    feat : 증감연산자 실습

---
+++ src/main/java/com/javaProject2/w2/d4/IncreaseOperator.java
@@ -0,0 +1,11 @@
+package com.javaProject2.w2.d4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        //10
+        //12
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+}
