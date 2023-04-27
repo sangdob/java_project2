dece49110dd04f6fd455a15a11378edbabb0177f
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-27 10:38:30 +0900

    feat : 비교연산자 실습 - 예제 2번

---
+++ src/main/java/com/javaProject2/w2/d4/ComparisonOperatorsEx2.java
@@ -0,0 +1,15 @@
+package com.javaProject2.w2.d4;
+
+public class ComparisonOperatorsEx2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("%d == %d --> %b\n", a, b, a == b);
+        System.out.printf("%d != %d --> %b\n", a, b, a != b);
+        System.out.printf("%d < %d --> %b\n", a, b, a < b);
+        System.out.printf("%d <= %d --> %b\n", a, b, a <= b);
+        System.out.printf("%d > %d --> %b\n", a, b, a > b);
+        System.out.printf("%d >= %d --> %b\n", a, b, a >= b);
+    }
+
+}
