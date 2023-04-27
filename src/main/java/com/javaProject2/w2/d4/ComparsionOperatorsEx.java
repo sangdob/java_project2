7454aae55539d7b468c6ba5764210a3e8883bc50
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-27 10:36:15 +0900

    feat : 비교연산자 실습

---
+++ src/main/java/com/javaProject2/w2/d4/ComparsionOperatorsEx.java
@@ -0,0 +1,10 @@
+package com.javaProject2.w2.d4;
+
+public class ComparsionOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+
+        System.out.println(iVal1 == iVal2);
+    }
+}
