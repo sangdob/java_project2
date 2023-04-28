55544f2d9b1fb418b5db51dcc8915628bb4b205b
sangdob <51118110+sangdob@users.noreply.github.com>
2023-04-28 13:18:13 +0900

    feat : switch case 문 실습

--- src/main/java/com/javaProject2/w2/d5/AutoSwap.java
+++ src/main/java/com/javaProject2/w2/d5/AutoSwap.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class AutoSwap {
     public static void main(String[] args) {
-        int[] arr = {2, 1, 7, 9};
-        boolean check = arr[0] > arr[1];
+            int[] arr = {2, 1, 7, 9};
+            boolean check = arr[0] > arr[1];
 
-        if (check) {
-            int temp = arr[0];
-            arr[0] = arr[1];
-            arr[1] = temp;
-        }
+            if (check) {
+                int temp = arr[0];
+                arr[0] = arr[1];
+                arr[1] = temp;
+            }
 
-        System.out.println(Arrays.toString(arr));
+            System.out.println(Arrays.toString(arr));
     }
 }
