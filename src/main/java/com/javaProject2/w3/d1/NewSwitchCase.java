624b64208c0a202378f7a5a03219980fa12d2474
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-01 10:09:21 +0900

    feat : [도전과제] 병원 시간표 실습

---
+++ src/main/java/com/javaProject2/w3/d1/NewSwitchCase.java
@@ -0,0 +1,15 @@
+package com.javaProject2.w3.d1;
+
+public class NewSwitchCase {
+    public static void main(String[] args) {
+        String day = "월";
+
+        String hour = switch (day) {
+            case "월", "화", "수", "목", "금" -> "09:00~18:30";
+            case "토" -> "09:00~18:30";
+            case "일" -> "쉬는날";
+        };
+
+        System.out.println(day + "요일 근무시간은 " + hour + "입니다.");
+    }
+}
