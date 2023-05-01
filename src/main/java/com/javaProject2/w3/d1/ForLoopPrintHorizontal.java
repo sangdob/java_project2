2ab8d98fe341622738211d569bf2bfc0a7d02baa
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-01 10:11:22 +0900

    feat : For 반복물 실습

---
+++ src/main/java/com/javaProject2/w3/d1/ForLoopPrintHorizontal.java
@@ -0,0 +1,10 @@
+package com.javaProject2.w3.d1;
+
+public class ForLoopPrintHorizontal {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+
+}
