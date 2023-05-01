046edc477cb543bc7de09887fb056b1e5b99848d
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-01 14:36:20 +0900

    feat : 구구단 실습

---
+++ src/main/java/com/javaProject2/w3/d1/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package com.javaProject2.w3.d1;
+
+public class MultiplicationTable {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d\n", 2 * i);
+        }
+    }
+}
---
+++ src/main/java/com/javaProject2/w3/d1/MultiplicationTableStep2.java
@@ -0,0 +1,9 @@
+package com.javaProject2.w3.d1;
+
+public class MultiplicationTableStep2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("2 * %d = %d\n", i, 2 * i);
+        }
+    }
+}
