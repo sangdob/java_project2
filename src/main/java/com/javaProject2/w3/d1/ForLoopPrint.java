9cd507b7d01bc7b2d477ede5eb23115c4e259f0a
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-01 10:18:13 +0900

    feat : For 반복문 실습  - 출력문 가로 배치형

---
+++ src/main/java/com/javaProject2/w3/d1/ForLoopPrint.java
@@ -0,0 +1,9 @@
+package com.javaProject2.w3.d1;
+
+public class ForLoopPrint {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf(i + " ");
+        }
+    }
+}
