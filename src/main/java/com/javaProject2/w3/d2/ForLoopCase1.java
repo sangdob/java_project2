b940fb6ff2db52630f24be0269a474976392cdcd
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-02 10:47:44 +0900

    feat : for 반복문 케이스 별 실습

---
+++ src/main/java/com/javaProject2/w3/d2/ForLoopCase1.java
@@ -0,0 +1,9 @@
+package com.javaProject2.w3.d2;
+
+public class ForLoopCase1 {
+    public static void main(String[] args) {
+        for (int i = 2; i == 1; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/com/javaProject2/w3/d2/ForLoopCase2.java
@@ -0,0 +1,9 @@
+package com.javaProject2.w3.d2;
+
+public class ForLoopCase2 {
+    public static void main(String[] args) {
+        for (int i = 2; 2 * i <= 18; i++) {
+            System.out.println(i);
+        }
+    }
+}
