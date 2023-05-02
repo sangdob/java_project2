40c6745314be5ba622d07911febd4f879d5f4798
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-02 09:47:48 +0900

    feat : switch case 오류 수정

--- src/main/java/com/javaProject2/w3/d1/NewSwitchCase.java
+++ src/main/java/com/javaProject2/w3/d1/NewSwitchCase.java
@@ -6,8 +6,9 @@ public class NewSwitchCase {
 
         String hour = switch (day) {
             case "월", "화", "수", "목", "금" -> "09:00~18:30";
-            case "토" -> "09:00~18:30";
+            case "토" -> "09:00~12:00";
             case "일" -> "쉬는날";
+            default -> "수 입력이 이상합니다.";
         };
 
         System.out.println(day + "요일 근무시간은 " + hour + "입니다.");
