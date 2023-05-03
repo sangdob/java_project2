dd03a30251d1c9b33683324ebffbffd12d3f0fa7
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-03 14:20:53 +0900

    feat : 소수 구하기 2/3 실습

---
+++ src/main/java/com/javaProject2/w3/d3/Prime3.java
@@ -0,0 +1,21 @@
+package com.javaProject2.w3.d3;
+
+import java.util.Scanner;
+
+public class Prime3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean flag = true;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        String printPrime = flag ? "소수입니다." : "소수가 아닙니다.";
+
+        System.out.println(printPrime);
+    }
+}
