ab67e0f2e2fe05181fd310414c5d55946f507b2e
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-03 13:09:25 +0900

    feat : 입력 한 수 소수 판별 실습

--- src/main/java/com/javaProject2/w3/d3/Prime.java
+++ src/main/java/com/javaProject2/w3/d3/Prime.java
@@ -8,11 +8,11 @@ public class Prime {
 
         int n = sc.nextInt();
 
-        boolean flag = false;
+        boolean flag = true;
 
         for (int i = 2; i < n; i++) {
-            if (n % i != 0) {
-                flag = true;
+            if (n % i == 0) {
+                flag = false;
                 break;
             }
         }
