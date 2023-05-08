cafa010d082dc06b07af6cc2649b44dc0f143d24
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-08 14:31:13 +0900

    feat : 메소드 실습 - 약수 더하기 메소드

---
+++ src/main/java/com/javaProject2/w4/d1/LionMath.java
@@ -0,0 +1,28 @@
+package com.javaProject2.w4.d1;
+
+public class LionMath {
+
+    public int sumOfFactors(int num) {
+        int sum = 1;
+
+        for (int i = num; i > 2; i--) {
+            if (num % i == 0) {
+                sum *= i;
+            }
+        }
+
+        return sum;
+    }
+
+    public void printResult(int num) {
+        System.out.println(num + "의 약수 합 : " + sumOfFactors(num));
+    }
+
+    public static void main(String[] args) {
+        LionMath math = new LionMath();
+        math.printResult(12);
+        math.printResult(10);
+        math.printResult(6);
+        math.printResult(20);
+    }
+}
