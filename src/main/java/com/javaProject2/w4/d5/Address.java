dff794781b9f979ddd99bb625d34c63d7d2fe301
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-12 13:52:44 +0900

    feat : address class 실습

---
+++ src/main/java/com/javaProject2/w4/d5/Address.java
@@ -0,0 +1,13 @@
+package com.javaProject2.w4.d5;
+
+public class Address {
+    private String fullAddr;
+    private String sido;
+    private String sigungu;
+
+    public Address(String fullAddr, String sido, String sigungu) {
+        this.fullAddr = fullAddr;
+        this.sido = sido;
+        this.sigungu = sigungu;
+    }
+}
