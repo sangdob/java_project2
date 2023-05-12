c2114969d352c170d13fdacbb650ff819848c138
sangdob <51118110+sangdob@users.noreply.github.com>
2023-05-12 13:52:30 +0900

    feat : hotspital 실습

---
+++ src/main/java/com/javaProject2/w4/d5/Hotspital.java
@@ -0,0 +1,13 @@
+package com.javaProject2.w4.d5;
+
+public class Hotspital {
+    private String name;
+    private String phoneNumber;
+    private Address address;
+
+    public Hospital(String name, String phoneNumber, Address address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+}
